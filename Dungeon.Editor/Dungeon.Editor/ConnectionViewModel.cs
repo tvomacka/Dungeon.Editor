1314db59c31e8392b17c343361489ecef4a9ce6e
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-09 22:59:20 +0100

    F!! ConnectionViewModel added

---
+++ Dungeon.Editor/Dungeon.Editor/ConnectionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Dungeon.Editor
+{
+    public class ConnectionViewModel
+    {
+        public ConnectorViewModel Source { get; set; }
+        public ConnectorViewModel Target { get; set; }
+    }
+}
