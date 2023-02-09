22a855553833715eed3e3ca50f888fadaf81a8ba
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-09 22:57:01 +0100

    F!! NodeViewModel added

--- Dungeon.Editor/Dungeon.Editor/NodeViewModel.cs
+++ Dungeon.Editor/Dungeon.Editor/NodeViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace Dungeon.Editor
+﻿using System.Collections.ObjectModel;
+
+namespace Dungeon.Editor
 {
     public class NodeViewModel
     {
         public string Title { get; set; }
+
+        public ObservableCollection<ConnectorViewModel> Input { get; set; } = new ObservableCollection<ConnectorViewModel>();
+        public ObservableCollection<ConnectorViewModel> Output { get; set; } = new ObservableCollection<ConnectorViewModel>();
     }
 }
