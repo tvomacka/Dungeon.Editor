c77f070069f2743a552cf85dd722a990e5e70a8d
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-09 23:00:17 +0100

    F!! connections added

--- Dungeon.Editor/Dungeon.Editor/EditorViewModel.cs
+++ Dungeon.Editor/Dungeon.Editor/EditorViewModel.cs
@@ -5,10 +5,11 @@ namespace Dungeon.Editor
     public class EditorViewModel
     {
         public ObservableCollection<NodeViewModel> Nodes { get; } = new ObservableCollection<NodeViewModel>();
+        public ObservableCollection<ConnectionViewModel> Connections { get; } = new ObservableCollection<ConnectionViewModel>();
 
         public EditorViewModel()
         {
-            Nodes.Add(new NodeViewModel
+            var welcome = new NodeViewModel
             {
                 Title = "Welcome",
                 Input = new ObservableCollection<ConnectorViewModel>
@@ -25,6 +26,27 @@ namespace Dungeon.Editor
                         Title = "Out"
                     }
                 }
+            };
+
+            var nodify = new NodeViewModel
+            {
+                Title = "To Nodify",
+                Input = new ObservableCollection<ConnectorViewModel>
+                {
+                    new ConnectorViewModel
+                    {
+                        Title = "In"
+                    }
+                }
+            };
+
+            Nodes.Add(welcome);
+            Nodes.Add(nodify);
+
+            Connections.Add(new ConnectionViewModel
+            {
+                Source = welcome.Output[0],
+                Target = nodify.Input[0]
             });
         }
     }
