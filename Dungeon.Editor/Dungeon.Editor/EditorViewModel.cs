4998177450dee8cd0a2ebcbdc8b83bdf40bbba19
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-09 22:46:46 +0100

    F!! EditorViewModel added

---
+++ Dungeon.Editor/Dungeon.Editor/EditorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Dungeon.Editor
+{
+    public class EditorViewModel
+    {
+        public ObservableCollection<NodeViewModel> Nodes { get; } = new ObservableCollection<NodeViewModel>();
+
+        public EditorViewModel()
+        {
+            Nodes.Add(new NodeViewModel { Title = "Welcome" });
+        }
+    }
+}
