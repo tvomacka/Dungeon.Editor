cc88fc94ffc3b4a68bedd9dbf16f0e64f87e28b7
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-09 23:11:37 +0100

    F!! constructor added

--- Dungeon.Editor/Dungeon.Editor/ConnectionViewModel.cs
+++ Dungeon.Editor/Dungeon.Editor/ConnectionViewModel.cs
@@ -2,7 +2,16 @@
 {
     public class ConnectionViewModel
     {
-        public ConnectorViewModel Source { get; set; }
-        public ConnectorViewModel Target { get; set; }
+        public ConnectionViewModel(ConnectorViewModel source, ConnectorViewModel target)
+        {
+            Source = source;
+            Target = target;
+
+            Source.IsConnected = true;
+            Target.IsConnected = true;
+        }
+
+        public ConnectorViewModel Source { get; }
+        public ConnectorViewModel Target { get; }
     }
 }
